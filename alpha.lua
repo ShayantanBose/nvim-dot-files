93e22c90037ecc9e889bc47bde9cb382e995b5de
ShayantanBose <shnbose13@gmail.com>
2025-01-13 22:52:10 +0530

    Added all the plugins

---
+++ alpha.lua
@@ -0,0 +1,23 @@
+return {
+  "goolord/alpha-nvim",
+  dependencies = {
+    "echasnovski/mini.icons",
+    "nvim-lua/plenary.nvim",
+  },
+  opts = function()
+    local dashboard = require("alpha.themes.theta")
+    local logo = [[
+                                                                           
+      	      ████ ██████           █████      ██                    
+      	     ███████████             █████                            
+      	     █████████ ███████████████████ ███   ███████████  
+      	    █████████  ███    █████████████ █████ ██████████████  
+      	   █████████ ██████████ █████████ █████ █████ ████ █████  
+      	 ███████████ ███    ███ █████████ █████ █████ ████ █████ 
+      	██████  █████████████████████ ████ █████ █████ ████ ██████
+    ]]
+
+    dashboard.config.header = vim.split(logo, "\n")
+    return dashboard.config
+  end,
+}
